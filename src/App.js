88d116274e876659bfdf56659f812253e4a8b9b8
Verdes-C <verdesconstantinadrian@gmail.com>
2023-09-14 12:22:32 +0300

    router fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import RootLayout from "./pages/Root";
-import AboutPage from "./pages/About";
-import EducationPage from "./pages/Education";
-import ExperiencePage from "./pages/Experience";
-import SkillsPage from "./pages/Skills";
-import InterestsPage from "./pages/Interests";
-import ErrorPage from "./pages/Error";
+import RootLayout from './pages/Root';
+import AboutPage from './pages/About';
+import EducationPage from './pages/Education';
+import ExperiencePage from './pages/Experience';
+import SkillsPage from './pages/Skills';
+import InterestsPage from './pages/Interests';
+import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/curriculum-vitae/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <AboutPage /> },
-      { path: "education", element: <EducationPage /> },
-      { path: "experience", element: <ExperiencePage /> },
-      { path: "skills", element: <SkillsPage /> },
-      { path: "interests", element: <InterestsPage /> },
+      { path: 'education', element: <EducationPage /> },
+      { path: 'experience', element: <ExperiencePage /> },
+      { path: 'skills', element: <SkillsPage /> },
+      { path: 'interests', element: <InterestsPage /> },
     ],
   },
 ]);
